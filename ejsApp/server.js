cc6fa7916215644eae0d8c118000b6e11816193c
Melissa Kipp <melissakipp.az@gmail.com>
2021-07-29 12:28:59 -0700

    Add ejs app env

---
+++ ejsApp/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+app.set('view-engine', 'ejs')
+app.get('/', (req, res) => {
+  res.render('index.ejs', { name: "Melissa" })
+})
+app.listen(3000, () => {
+  console.log('app listening on port 3000');
+});
+
+
+// Bret Fisher: Docker for Node.js course Video 25
+// quit on ctrl-c when running docker in terminal
+process.on("SIGINT", function onSigint() {
+  console.info("Got SIGINT (aka ctrl-c in docker). Graceful shutdown ", new Date().toISOString());
+  shutdown();
+});
+
+// quit properly on docker stop
+process.on("SIGTERM", function onSigterm() {
+  console.info("Got SIGTERM (docker container stop). Graceful shutdown ", new Date().toISOString());
+  shutdown();
+});
+
+// shut down server
+function shutdown() {
+  // NOTE: server.close is for express based apps
+  // If using hapi, use `server.stop`
+  server.close(function onServerClosed(err) {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+    }
+    process.exit();
+  })
+};
+// END Bret Fisher
